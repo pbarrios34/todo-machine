508e86f1e7d56bfd29952034c5fab6ad967afd4a
Paolo Barrios <barrios.paolo1993@gmail.com>
2024-10-29 23:56:32 -0600

    [UPDATE] useContext() Clase 22 y 23 Bug:Completar y Eliminar ToDo

--- src/TodoImage.js
+++
@@ -1,9 +0,0 @@
-function TodoImage() {
-    return (
-        <div className="image">
-            <img src="https://cdn3d.iconscout.com/3d/premium/thumb/lista-de-escritura-7442174-6136664.png?f=webp" alt="logo"/>
-        </div>
-    );
-  }
-
-export { TodoImage };
