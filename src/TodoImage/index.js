3cf2cccbf3a4f7dd617b652c247aa58b3ba62c40
Paolo Barrios <barrios.paolo1993@gmail.com>
2024-11-02 22:04:19 -0600

    Create responsive styles

--- src/TodoImage/index.js
+++ src/TodoImage/index.js
@@ -1,7 +1,9 @@
+import './TodoImage.css'
+
 function TodoImage() {
     return (
-        <div className="image">
-            <img src="https://cdn3d.iconscout.com/3d/premium/thumb/lista-de-escritura-7442174-6136664.png?f=webp" alt="logo"/>
+        <div>
+            <img className="image" src="https://cdn3d.iconscout.com/3d/premium/thumb/lista-de-escritura-7442174-6136664.png?f=webp" alt="logo"/>
         </div>
     );
   }
